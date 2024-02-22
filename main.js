4a760da5d9d4be3c4a19c4c1e82f903e0a8f7076
FatimaGR <fawicoma@gmail.com>
2024-02-22 14:17:30 -0500

    Added home styles, responsive menu and dark mode corrections

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const body = document.querySelector("body");
-const darkMode = document.querySelector("#mode-button");
+const darkMode = document.querySelector(".mode-button");
+const optionsButton = document.querySelector(".options-button");
+const options = document.querySelector(".options");
 
 darkMode.addEventListener("click", () => {
   body.classList.toggle("dark-mode");
 });
+
+optionsButton.addEventListener("click", () => {
+  options.classList.toggle("visible");
+})
